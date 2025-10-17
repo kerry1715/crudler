e2c83dce18aa793600ef09c9204eac91a1a0a50e
kerry1715 <conorhealy567@gmail.com>
2025-10-17 16:40:36 +0100

    Populated ModuleList and made data pressable

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,23 +1,45 @@
-import { StyleSheet, Text } from 'react-native';
 import Screen from "../layout/Screen";
+import { Pressable, StyleSheet } from "react-native";
+import {modules as initialModules} from '../../data/modules';
+import { ScrollView, View } from 'react-native-web';
 
 export const ModuleListScreen = () => {
   // Initialisations ---------------------
+  const modules = initialModules;
   // State -------------------------------
   // Handlers ----------------------------
+  const handleSelect = () => alert('Item selected');
   // View --------------------------------
   return (
-    <Screen style={styles.container}>
-      <Text>List</Text>
+    <Screen>
+      <ScrollView style={styles.container}>
+      {
+        modules.map((module) => {
+          return (
+            <Pressable onPress={handleSelect}>
+              <View style={styles.item}>
+                <text style={styles.text}>
+                  {module.ModuleCode} {module.ModuleName}
+                </text>
+              </View>
+            </Pressable>
+          );
+        })
+      }
+      </ScrollView>
     </Screen>
   );
 };
-export default ModuleListScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgray',
+  },
+  text: {
+    fontSize: 16,
   },
 });
+export default ModuleListScreen;
